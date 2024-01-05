ad55e242d54469b45104efda6a8f173851ec6062
Joao <vitorjoao.cl@gmail.com>
2024-01-04 22:10:14 -0300

    first commit

---
+++ Migrations/20240104171420_novo-campo-teste.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCFilmes.Migrations
+{
+    /// <inheritdoc />
+    public partial class novocampoteste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        
+
+            migrationBuilder.AddColumn<int>(
+                name: "Pontos",
+                table: "Filmes",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Pontos",
+                table: "Filmes");
+
+           
+        }
+    }
+}
---
+++ Models/Filmes.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCFilmes.Models
+{
+    public class Filmes
+    {
+        public int ID { get; set; }
+
+        [MaxLength(100)]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        public string Titulo { get; set; } = string.Empty;
+        [Display(Name = "Data de lançamento")]
+
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+
+        [DataType(DataType.Date)]
+        public DateTime DataLancamento { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo é obrigatório"),
+        StringLength(50)]
+        public string Genero { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal 18, 2")]
+        [DataType(DataType.Currency)]
+        public decimal Preco { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(0,5)]
+        public int? Pontos { get; set; }
+
+
+    }
+}
